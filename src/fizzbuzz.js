79bf3dae670d000ccff5186fcabfa321b6bf281b
Israel Antezana Rojas <israelantezana@gmail.com>
2024-11-19 10:23:51 -0400

    Refactoring: Se agrupa funciones en clase

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,29 +1,30 @@
-function generarFizzBuzzDe(numero) {
-  const FIZZ = 3;
-  const BUZZ = 5;
-  if (esMultiplo(numero, FIZZ) && esMultiplo(numero, BUZZ)) {
-    return "FizzBuzz";
+class FizzBuzz {
+  generarFizzBuzzDe(numero) {
+    const FIZZ = 3;
+    const BUZZ = 5;
+    if (this.esMultiplo(numero, FIZZ) && this.esMultiplo(numero, BUZZ)) {
+      return "FizzBuzz";
+    }
+    if (this.esMultiplo(numero, FIZZ)) {
+      return "Fizz";
+    }
+    if (this.esMultiplo(numero, BUZZ)) {
+      return "Buzz";
+    }
+    return numero.toString();
   }
-  if (esMultiplo(numero, FIZZ)) {
-    return "Fizz";
-  }
-  if (esMultiplo(numero, BUZZ)) {
-    return "Buzz";
-  }
-  return numero.toString();
-}
 
-function fizzbuzz(n) {
-  let aux = "";
-  for (let i = 1; i <= n; i++) {
-    aux += generarFizzBuzzDe(i);
-    if (i != n) aux += ", ";
+  fizzbuzz(n) {
+    let aux = "";
+    for (let i = 1; i <= n; i++) {
+      aux += this.generarFizzBuzzDe(i);
+      if (i != n) aux += ", ";
+    }
+    return aux;
   }
-  return aux;
-}
 
-function esMultiplo(numero, multiplo) {
-  return numero % multiplo === 0;
+  esMultiplo(numero, multiplo) {
+    return numero % multiplo === 0;
+  }
 }
-
-export { generarFizzBuzzDe, fizzbuzz };
+export default FizzBuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,37 +1,44 @@
-import { generarFizzBuzzDe, fizzbuzz } from "./fizzbuzz";
+import FizzBuzz from "./fizzbuzz";
 
 describe("Fizzbuzz de un numero", () => {
   it("Numero sin regla", () => {
-    expect(generarFizzBuzzDe(1)).toEqual("1");
+    let fb = new FizzBuzz();
+    expect(fb.generarFizzBuzzDe(1)).toEqual("1");
   });
 
   it("Regla de Fizz para 3", () => {
-    expect(generarFizzBuzzDe(3)).toEqual("Fizz");
+    let fb = new FizzBuzz();
+    expect(fb.generarFizzBuzzDe(3)).toEqual("Fizz");
   });
 
   it("Regla de Buzz para 5", () => {
-    expect(generarFizzBuzzDe(5)).toEqual("Buzz");
+    let fb = new FizzBuzz();
+    expect(fb.generarFizzBuzzDe(5)).toEqual("Buzz");
   });
 
   it("Regla de Fizz para multiplos de 3", () => {
-    expect(generarFizzBuzzDe(6)).toEqual("Fizz");
-    expect(generarFizzBuzzDe(9)).toEqual("Fizz");
+    let fb = new FizzBuzz();
+    expect(fb.generarFizzBuzzDe(6)).toEqual("Fizz");
+    expect(fb.generarFizzBuzzDe(9)).toEqual("Fizz");
   });
 
   it("Regla de Buzz para multiplos de 5", () => {
-    expect(generarFizzBuzzDe(10)).toEqual("Buzz");
-    expect(generarFizzBuzzDe(20)).toEqual("Buzz");
+    let fb = new FizzBuzz();
+    expect(fb.generarFizzBuzzDe(10)).toEqual("Buzz");
+    expect(fb.generarFizzBuzzDe(20)).toEqual("Buzz");
   });
 
   it("Regla FizzBuzz para multiplos de 3 y 5", () => {
-    expect(generarFizzBuzzDe(15)).toEqual("FizzBuzz");
-    expect(generarFizzBuzzDe(30)).toEqual("FizzBuzz");
+    let fb = new FizzBuzz();
+    expect(fb.generarFizzBuzzDe(15)).toEqual("FizzBuzz");
+    expect(fb.generarFizzBuzzDe(30)).toEqual("FizzBuzz");
   });
 
   describe("Fizzbuzz de una secuencia", () => {
     it("Genera la secuencia hasta el numero especificado", () => {
-      expect(fizzbuzz(3)).toEqual("1, 2, Fizz");
-      expect(fizzbuzz(5)).toEqual("1, 2, Fizz, 4, Buzz");
+      let fb = new FizzBuzz();
+      expect(fb.fizzbuzz(3)).toEqual("1, 2, Fizz");
+      expect(fb.fizzbuzz(5)).toEqual("1, 2, Fizz, 4, Buzz");
     });
   });
 });
